9be463200b39bf27752f1e1482ce66122012b6a3
Eva Freire Suárez <evafreiresuarez@gmail.com>
2025-10-06 21:48:15 +0200

    lo bueno está en ET1

---
+++ ET1/ET1_Eva_FreireSuarez.js
@@ -0,0 +1,11 @@
+// Fichero: ET1_Eva_Gonzalez.js
+// Variable con datos generales de la entrega
+// Formato requerido:
+// datosgenerales = Array(Apellido1 Apellido2 Nombre (del Alumno), entrega, horasdedicadas);
+
+var datosgenerales = [
+  "Freire Suárez Eva",
+  "ET1_InterfacesUsuario",
+  30
+];
+
